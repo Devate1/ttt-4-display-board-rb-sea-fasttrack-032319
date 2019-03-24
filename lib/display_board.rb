825026d2dc1d2935b47b27255445e17ccef979f5
Matt Hughes <mattw.hughes@gmail.com>
2019-03-24 07:35:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
-  puts "#{board0}|#{board1}|#{board2}"
+  puts "#{board[0]}|#{board[1]}|#{board[2]}"
   puts "-----------"
-  puts "#{board3}|#{board4}|#{board5}"
+  puts "#{board[3]}|#{board[4]}|#{board[5]}"
   puts "-----------"
-  puts "#{board6}|#{board7}|#{board8}"
+  puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
